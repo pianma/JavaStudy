3d08050f2b24bdc0417ade52936e35def1c229bc
kkw2040 <ikkw2040@naver.com>
2021-03-15 17:19:06 +0900

    최초 프로젝트 업로드

---
+++ src/chapter1/Ex1.java
@@ -0,0 +1,20 @@
+package chapter1;
+
+public class Ex1 {
+//클래스 : 소스 코드의 최소 단위
+// 메인 함수 : 시작점, 내가 작성한 소소코드의 시작이 어딘지 알려주는 기능
+	
+	public static void main(String[] args) {
+		//주석 : 메모, 개발자가 참고해야할 사항
+		
+		System.out.println("Hello World~!"); 
+		System.out.println("주석 연습 중...."); 
+		System.out.println("출력1");
+		System.out.println("sysout+ ctrl + space"); //자동완성
+		// 무언가를 출력하기 위한 기능
+		// ()안에다가 출력 대상을 지정
+		//System.out.println("출력1"); //소스코드를 주석처리하면 실행되지 않음
+		
+//		System.out.println("ctrl+shift+c"); //주석처리 자동완성
+	}
+}
---
+++ src/chapter1/Ex2.java
@@ -0,0 +1,33 @@
+package chapter1;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+//	System.out.println("Hello World~!");
+	//문자열 - 문자의 나열
+	//문자열은 꼭 쌍따옴표를 사용해서 표현해야한다.
+	
+//	System.out.println("Hello");
+//	System.out.println("world~!");
+//	System.out.print("I ");
+//	System.out.print("am ");
+//	System.out.print("a Java Programmer");
+//	System.out.print("오늘은 ");
+//	System.out.print("자바 수업 ");
+//	System.out.print("첫 번째 날입니다.");
+//		System.out.println(1);
+//		//정수
+//		System.out.println(3.14);
+//		//실수
+//		System.out.println('a');
+//		//문자
+//		System.out.println("abcd");
+//		//문자열
+		
+	System.out.print("오늘의 기온 : ");
+	System.out.print(-5.3);
+	System.out.println('c');
+	
+	System.out.println("오늘의 기온 : " + -5.3 + 'c');
+}
+	}
